478f1214688aa95e5094d9cba2a5ff5c98c7e793
Reaz <reaz.rahman@macaulay.cuny.edu>
2021-04-11 10:30:12 -0400

    Delete class called in timetester

--- TimeTester.java
+++ TimeTester.java
@@ -2,14 +2,14 @@ import java.text.*;
 import java.util.*;
 
 public class TimeTester {
-	static TollAccumulator myTollAcc;
-	// Creating date format
+    static TollAccumulator myTollAcc;
+    // Creating date format
     static final DateFormat simple = new SimpleDateFormat("dd MMM yyyy HH:mm:ss");
 
-	public static void main(String args[]) {
-		myTollAcc = new TollAccumulator();
+    public static void main(String args[]) {
+        myTollAcc = new TollAccumulator();
 
-		System.out.println("------------------- DATA STRUCTURE GENERATION -------------------------");
+        System.out.println("------------------- DATA STRUCTURE GENERATION -------------------------");
         long start;
         long end;
         int size=100000;
@@ -19,25 +19,25 @@ public class TimeTester {
         List<Toll> tollList=myTollAcc.populateTollList(size, tollArray);
         end = System.nanoTime();
         System.out.println("Took " + (end - start) + " ns to generate "+ size +" tolls with an Array List");
-        
+
         List<Toll> tollLinked = new LinkedList<>();
         start = System.nanoTime();
         myTollAcc.populateTollList(size, tollLinked);
         end = System.nanoTime();
         System.out.println("Took " + (end - start) + " ns to generate "+ size +" tolls with an Linked List");
-        
+
         Map<Long, Toll> tollMap = new Hashtable<>();
         start = System.nanoTime();
         myTollAcc.populateTollMap(size, tollMap);
         end = System.nanoTime();
         System.out.println("Took " + (end - start) + " ns to generate "+ size +" tolls with a Hash Table");
-        
+
         // Creating date from milliseconds
         // using Date() constructor
         Date result = new Date(myTollAcc.generateStartingTimeStamp());
-  
+
         // Formatting Date according to the given format
-        // Test code to print out start date and ensure that it was 
+        // Test code to print out start date and ensure that it was
         // built as intended
         //System.out.println(simple.format(result));
 
@@ -45,6 +45,11 @@ public class TimeTester {
         System.out.println("------------------- SEARCH TIMER ---------------------------");
         TollSearch mySearch= new TollSearch();
         mySearch.SearchRankerAlternate(tollArray,tollLinked,tollMap);
+
+        System.out.println("------------------- DELETE TIMER ---------------------------");
+        TollDelete myDelete= new TollDelete();
+        myDelete.DeleteRanker(tollArray,tollLinked,tollMap);
+
         /// REAZ (END) ///
-	}
+    }
 }
